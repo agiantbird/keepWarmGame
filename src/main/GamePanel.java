fb5b6f9e344f0d36ba963aab8d000759c02f9f9c
Richard Hoyt <rhoyt10@gmail.com>
2023-08-05 11:21:35 -0400

    Add game loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,10 +1,9 @@
 package main;
 
 import javax.swing.JPanel;
-import java.awt.Dimension;
-import java.awt.Color;
+import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     // SCREEN SETTINGS
     final int originalTileSize = 16; // 16x16 tile
     final int scale = 3;
@@ -14,9 +13,41 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol; // 768 pixels wide
     final int screenHeight = tileSize * maxScreenRow; // 576 pixels high
 
+    Thread gameThread;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            // UPDATE: update information such as character pos
+            update();
+            // DRAW: draw the screen with updated information
+            // paintComponent() actually called with repaint()
+            repaint();
+        }
+    }
+
+    public void update() {
+
+    }
+
+    // paintComponent is a standard JPanel method
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.white);
+        g2.fillRect(100, 100, tileSize, tileSize);
+        g2.dispose();
     }
 }

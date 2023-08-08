a7c66fd5f60e98fa6b0f757446736c21b2858e00
Richard Hoyt <rhoyt10@gmail.com>
2023-08-07 22:40:52 -0400

    Implement collision detection

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
 
 /**
@@ -12,4 +13,6 @@ public class Entity {
     public String direction;
     public int spriteCounter = 0;
     public int spriteNum = 1;
+    public Rectangle solidArea;
+    public boolean collisionOn = false;
 }
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -28,6 +28,7 @@ public class GamePanel extends JPanel implements Runnable {
     TileManager tileManager = new TileManager(this);
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    public CollisionChecker collisionChecker = new CollisionChecker(this);
     public Player player = new Player(this, keyH);
 
     public GamePanel() {

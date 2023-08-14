8e1f56f5ecb8e934b1aa45ea960fe412147189c1
Richard Hoyt <rhoyt10@gmail.com>
2023-08-13 20:58:51 -0400

    CODE-11 Improve rendering performance

--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -1,13 +1,19 @@
 package object;
 
+import main.GamePanel;
+
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
 public class OBJ_Door extends SuperObject{
-    public OBJ_Door() {
+
+    GamePanel gp;
+
+    public OBJ_Door(GamePanel gp) {
         name = "Door";
         try {
             image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("objects/door.png"));
+            utilityTool.scaleImage(image, gp.tileSize, gp.tileSize);
         } catch(IOException e) {
             e.printStackTrace();
         }
--- src/object/OBJ_Lantern.java
+++ src/object/OBJ_Lantern.java
@@ -1,13 +1,19 @@
 package object;
 
+import main.GamePanel;
+
 import javax.imageio.ImageIO;
 import java.io.IOException;
 
 public class OBJ_Lantern extends  SuperObject{
-    public OBJ_Lantern() {
+
+    GamePanel gp;
+
+    public OBJ_Lantern(GamePanel gp) {
         name = "Lantern";
         try {
             image = ImageIO.read(getClass().getClassLoader().getResourceAsStream("objects/pine_cone_lantern.png"));
+            utilityTool.scaleImage(image, gp.tileSize, gp.tileSize);
         } catch(IOException e) {
             e.printStackTrace();
         }
